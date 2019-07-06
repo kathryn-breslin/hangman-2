240f3a7fd8cf91f60fda6cf4bbece9c4d930cd0f
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-06 10:40:34 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -75,6 +75,24 @@ function checkLetters(letter) {
 
 function scoring () {
     console.log("Win Count:" + winCount + " | Loss count: " + lossCount + " | Guesses Left: " + guessesLeft);
+
+    document.getElementById('numGuesses').innerHTML = guessesLeft;
+    document.getElementById('wordToGuess').innerHTML = blanksAndGuesses.join(" ");
+    document.getElementById('wrongGuesses').innerHTML = wrongGuesses.join(" ")
+    //Check if user won
+    if (lettersInWord.toString() === blanksAndGuesses.toString()) {
+        winCount++;
+        console.log("You won!");
+        document.getElementById('winCounter').innerHTML = winCount;
+        startGame();
+    }
+    //Check if user lost
+    else if (guessesLeft === 0){
+        lossCount++;
+        console.log("You lost!");
+        document.getElementById('lossCounter').innerHTML = lossCount;
+        startGame();
+    }
 }
 // MAIN PROCESS
 //===================================================================
