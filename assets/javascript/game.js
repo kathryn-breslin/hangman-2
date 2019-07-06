873fe99cf9d8bfa08e3e6c0dd54e489ea85cf85a
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-06 08:58:44 -0400

    setting up html and js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//GLOBAL VARIABLES
+//===================================================================
+// Array and Variables for holding data
+
+var moonOptions = ["new moon", "waxing crescent", "first quarter", "waxing gibbous", "full moon", "waning gibbous", "third quarter", "waxing crescent"];
+var selectedWord = "";
+var lettersInWord = [];
+var numberOfBlanks = 0;
+var blanksAndGuesses = [];
+var wrongGuesses = [];
+
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+//FUNCTIONS
+//===================================================================
+
+
+//MAIN PROCESS
+//===================================================================
